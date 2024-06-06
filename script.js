13a2ead9d72885654711c9a4abfbab642ed5ee5f
JamesonStansfield <159183491+JamesonStansfield@users.noreply.github.com>
2024-06-06 14:16:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 /**
  * This file will get loaded by index.html
  */
+'use strict';
+
+//Global Click Counter
+let clicks = 0;
+
+//Adding Clicks
+document.getElementById("clickButton").addEventListener("click", updateClicks)
+
+function updateClicks() {
+  clicks++;
+  document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
+}
+
+//Subtracting Clicks
+document.getElementById("clickReduce").addEventListener("click", reduceClicks)
+
+function reduceClicks() {
+  clicks--;
+  document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
+}
+
+//Reseting Clicks
+document.getElementById("clickReset").addEventListener("click", resetClicks)
+
+function resetClicks() {
+  clicks = 0;
+  document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
+}
