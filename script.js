46420c214f222522c84980933f350a36cabd1b02
Jameson Stansfield <jameson.stansfield@stu.ocsb.ca>
2024-06-07 14:20:58 -0400

    Clickrate and upgrade added

--- script.js
+++ script.js
@@ -5,12 +5,14 @@
 
 //Global Click Counter
 let clicks = 0;
+let clickRate = 1;
+let x2 = false;
 
 //Adding Clicks
 document.getElementById("clickButton").addEventListener("click", updateClicks)
 
 function updateClicks() {
-  clicks++;
+  clicks += clickRate;
   document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
 }
 
@@ -23,9 +25,32 @@ function reduceClicks() {
 }
 
 //Reseting Clicks
+document.getElementById("clickReset").style.backgroundColor = "#f5a4a4";
+
 document.getElementById("clickReset").addEventListener("click", resetClicks)
 
 function resetClicks() {
   clicks = 0;
   document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
 }
+
+//First Upgrade
+
+document.getElementById("upgrade1").addEventListener("click", checkx2);
+
+function checkx2() {
+  if (clicks >= 50) {
+    if (x2 == false) {
+      clicks -= 50;
+      document.getElementById("upgrade1").textContent = "Upgraded";
+      document.getElementById("upgrade1").style.backgroundColor = "#8fe87c";
+      document.getElementById("clickButton").textContent = `Clicks: ${clicks}`;
+      x2 = true;
+      clickRate = 2;
+    }
+  }
+}
+
+
+//Idea notes for author:
+//display a new click rate increase button after the second
